167526cc32da935a8370c7e6d5ade64cfc6df463
Alaa <angnm38@gmail.com>
2025-11-09 18:11:03 +0100

    Fix prep-ex week2 code

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -2,16 +2,26 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
+
 const port = 3000;
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//handle errors
+const handleError = (res, statusCode, msg) => {
+  return res.status(statusCode).json({ msg: msg });
+};
 
 //create a blog
 app.post("/blogs", (req, res) => {
   const title = req.body.title;
   const content = req.body.content;
+
   if (!title || !content) {
-    return res.status(400).json({ msg: "Missing data." });
+    return handleError(res, 400, "Missing data.");
   }
+
   fs.writeFileSync(title, content);
   res.end("ok");
 });
@@ -21,39 +31,33 @@ app.put("/blogs/:title", (req, res) => {
   const title = req.params.title;
   const content = req.body.content;
 
-  if (fs.existsSync(title)) {
-    if (!content) {
-      return res.status(400).json({ msg: "Missing data." });
-    }
-    fs.writeFileSync(title, content);
-    res.end("ok");
-  } else {
-    // Send response with error message
-    res.status(404).json({ msg: "File not found" });
+  if (!fs.existsSync(title)) {
+    return handleError(res, 404, "File not found");
   }
+
+  if (!content) return handleError(res, 400, "Missing data.");
+  fs.writeFileSync(title, content);
+  res.end("ok");
 });
 
 //delete a blog
 app.delete("/blogs/:title", (req, res) => {
   const title = req.params.title;
-  // How to get the title from the url parameters?
-  if (fs.existsSync(title)) {
-    // Add condition here
-    fs.unlinkSync(title);
-    res.end("ok");
-  } else {
-    res.status(404).json({ msg: "File not found" });
-    // Respond with message here
+
+  if (!fs.existsSync(title)) {
+    return handleError(res, 404, "File not found");
   }
+
+  fs.unlinkSync(title);
+  res.end("ok");
 });
 
 //read a blog
 app.get("/blogs/:title", (req, res) => {
   const title = req.params.title;
-  // How to get the title from the url parameters?
   // check if post exists
   if (!fs.existsSync(title)) {
-    return res.status(404).json({ msg: "File not found" });
+    return handleError(res, 404, "File not found");
   }
   // send response
   const post = fs.readFileSync(title);
@@ -63,18 +67,15 @@ app.get("/blogs/:title", (req, res) => {
 //read all blogs
 app.get("/blogs", (req, res) => {
   const files = fs.readdirSync(__dirname);
-
   const blogs = files
     .map((file) => {
       const filename = path.join(__dirname, file);
       const stats = fs.lstatSync(filename);
-
       if (stats.isFile() && !path.extname(filename)) {
         return { title: file };
       }
     })
     .filter(Boolean);
-
   res.status(200).json(blogs);
 });
 
